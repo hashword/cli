c7ff0eeaad5e384431b6a667efc8d0dad6ff7fe7
Cody Scott <cody.j.b.scott@gmail.com>
2015-12-06 14:33:50 -0500

    Fixing source file path

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import ast
+import os
 import re
 from setuptools import setup, find_packages
 
 
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 def get_version():
     _version_re = re.compile(r'VERSION\s+=\s+(.*)')
-    with open('src/hashword/hashword.py', 'rb') as f:
+    source_file = os.path.join(THIS_DIR, 'src', 'hashword', 'hashword.py')
+    with open(source_file, 'rb') as f:
         version = str(ast.literal_eval(_version_re.search(
             f.read().decode('utf-8')).group(1)))
     return version
